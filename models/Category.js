ebe2632ee70737efca405e2fa7b868ec8f0e3d74
MMK <staph103@gmail.com>
2024-01-05 23:14:05 -0500

    first commit worked on Category get call and table creation

---
+++ models/Category.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Category extends Model {}
+
+Category.init(
+  {
+    // define columns
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primarKey: true,
+      autoIncrement: true
+    },
+    category_name:{
+
+    }
+
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'category',
+  }
+);
+
+module.exports = Category;
+// Location.init(
+//   {
+//     id: {
+//       type: DataTypes.INTEGER,
+//       allowNull: false,
+//       primaryKey: true,
+//       autoIncrement: true
+//     },
+//     location_name: {
+//       type: DataTypes.STRING,
+//       allowNull: false
+//     }
+//   },
